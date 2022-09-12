393350ef047f03d0284e902ba10daa68528ed75d
arieldonner <arieldonner@gmail.com>
2022-09-12 20:15:44 +0000

    Created html and css structure. Started working on issue 1. Added listen for input events. Added listen for submit events.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var $photoURL = document.querySelector('#photo-url');
+var $img = document.querySelector('img');
+
+$photoURL.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  $img.setAttribute('src', event.target.value);
+}
+
+var $entryForm = document.querySelector('#entry-form');
+
+$entryForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var values = {
+    title: document.forms[0].elements.title.value,
+    photo: document.forms[0].elements['photo-url'].value,
+    notes: document.forms[1].elements.notes.value
+  };
+
+  values.nextEntryId = 0;
+  values.nextEntryId += 1;
+  data.entries.push(values);
+
+  $img.setAttribute('src', '../images/placeholder-image-square.jpg');
+
+  document.forms[0].reset();
+  document.forms[1].reset();
+}
